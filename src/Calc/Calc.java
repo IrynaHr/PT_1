e9ef09353e0997a778088864a6ee0f6600a57728
Iryna Hrytsai <irynahrytsai@gmail.com>
2018-11-12 06:41:37 +0200

    Add a project

---
+++ src/Calc/Calc.java
@@ -0,0 +1,25 @@
+package Calc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calc {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter a");
+		int a = Integer.parseInt(br.readLine());
+		System.out.println("Enter b");
+		int b = Integer.parseInt(br.readLine());
+
+		System.out.println("a+b = " + (a + b));
+		System.out.println("a-b = " + (a - b));
+		System.out.println("a*b = " + (a * b));
+		System.out.println("a/b = " + (a / b));
+
+	}
+
+}
